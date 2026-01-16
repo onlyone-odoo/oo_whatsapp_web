e13eaef2e206ddbc6935e931dc35d8bf577358ce
onlyone-odoo <matiasb@onlyone.com.ar>
2026-01-15 22:13:17 -0300

    Fix: Change Text fields to Char (Text not supported in res.config.settings)

--- models/settings.py
+++ models/settings.py
@@ -4,15 +4,15 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    sale_message = fields.Text(
+    sale_message = fields.Char(
         string="Mensaje de Venta",
         config_parameter="oo_whatsapp_web.sale_message",
     )
-    invoice_message = fields.Text(
+    invoice_message = fields.Char(
         string="Mensaje de Factura",
         config_parameter="oo_whatsapp_web.invoice_message",
     )
-    purchase_message = fields.Text(
+    purchase_message = fields.Char(
         string="Mensaje de Compra",
         config_parameter="oo_whatsapp_web.purchase_message",
     )
